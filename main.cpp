6fd348dae350d03a7f70a1bc22de526043581cfe
JacksonRankin117 <ran23008@byui.edu>
2026-06-05 12:07:01 -0600

    Made a 2K render, 4000 samples/pixel

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <vector>
 #include <memory>
 #include "camera.h"
+#include "math_objects.h"
 #include "scene.h"
 #include "object.h"
 #include "material.h"
 #include "spd.h"
 
 int main() {
+    // ===================================================== Scene =====================================================
     // Materials using measured Macbeth SPDs
     Lambertian groundMat(&SPD::Gray());        // Ideal matte grey
     Lambertian redMat(&SPD::Red());            // Ideal matte red
-    Lambertian blueMat(&SPD::Blue());    // Ideal matte blue
+    Lambertian blueMat(&SPD::Blue());          // Ideal matte blue
     Lambertian greenMat(&SPD::Green());        // Ideal matte green
     Metallic   metalMat(&SPD::White(), 0.05);  // Polished silver
     Dielectric glassMat(1.5220, 0.00459);      // Crown glass (1.5220, 0.00459)
@@ -23,16 +25,36 @@ int main() {
     objects.push_back(std::make_unique<Sphere>(Math::Vec3( 0.75,  1.0,  0.5),    0.5,   &metalMat));
     objects.push_back(std::make_unique<Sphere>(Math::Vec3( 0.00,  0.0, -1000.0), 1000.0, &groundMat));
 
-    Pinhole cam(
-        2160, 1440,
-        Math::Vec3(0, -4,  1),
-        Math::Vec3(0,  0,  0),
-        Math::Vec3(0,  0,  1),
-        40.0,
-        4000            // 16,000 samples needed for production quality, :(
-    );
+    // ==================================================== Camera =====================================================
+
+
+    // Image dimensions
+    int width  = 2160;
+    int height = 1440;
+
+    // Camera attributes
+    Math::Vec3     position(0, -4,  1);
+    Math::Vec3       target(0,  0,  0);
+    Math::Vec3 up_direction(0,  0,  1);
+
+    // Field-of-view
+    double fov = 40.0;
+
+    // Number of samples per pixel (250 for quick render, 10,000+ for production-quality)
+    int samples = 4'000;
+
+    int ray_depth = 20;
+
+    // Camera object construction
+    Pinhole cam(width,
+                height,
+                position,
+                target,
+                up_direction,
+                fov,
+                samples);
 
     Scene scene(cam, std::move(objects));
-    scene.Render("render.pfm", 20);
+    scene.Render("render.pfm", ray_depth);
     return 0;
 }
